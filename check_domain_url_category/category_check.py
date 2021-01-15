93b9eadaf6936a9a1c1c2375fe15b9a11cb001e6
Anna Barone <abarone@paloaltonetworks.com>
2021-01-15 15:06:12 -0800

    Combined url_check.py and domain_check.py into one

--- check_domain_url_category/category_check.py
+++ check_domain_url_category/category_check.py
@@ -15,8 +15,66 @@
 
 # Authors: Scott Shoaf and Anna Barone
 
+import csv
+import json
 import click
 from skilletlib import Panos
+
+# Queries the PANW firewall to gain Domain Category for given item domain
+def query_domain(item, device):
+    category = list()
+    response = ""
+    category_list = ['benign', 'malware', 'command-and-control', 'phishing', 'dynamic-dns', 'newly-registered-domain',
+                     'grayware', 'parked', 'proxy-avoidance-anonymizers', 'tbd9', 'tbd10']
+    cli_cmd = f'<test><dns-proxy><dns-signature><fqdn>{item}</fqdn></dns-signature></dns-proxy></test>'
+
+    # Query the device object to get the domain category
+    try:
+        response = device.execute_op(cmd_str=cli_cmd, cmd_xml=False)
+    except Exception as e:
+        print(f"Error: Problem when executing the op: {e}")
+        exit()
+
+    # Parse the response for category
+    dns_data = json.loads(response)
+    category_num = dns_data['dns-signature'][0]['category']
+    dns_category = category_list[category_num]
+    print(f'{item}, {dns_category}')
+
+    # Save (domain, category) pairing in output list
+    category.append(item)
+    category.append(dns_category)
+
+    return category
+
+
+# Queries the PANW firewall to gain URL Category for given item domain
+def query_url(item, device):
+    category = list()
+    response = list()
+
+    # Query the device object to get the url category
+    cli_cmd = f'<test><url>{item}</url></test>'
+    try:
+        response = device.execute_op(cmd_str=cli_cmd, cmd_xml=False).split('\n')
+    except Exception as e:
+        print(f"Error: Problem when executing the op: {e}")
+
+    # Parse the response for local and cloud classification
+    local = response[0]
+    cloud = response[1]
+
+    # Parse classification for category and risk
+    category_local, risk_local = local.split(" ")[1], local.split(" ")[2]
+    category_cloud, risk_cloud = cloud.split(" ")[1], cloud.split(" ")[2]
+    category.append(item)
+    category.append(category_cloud)
+    category.append(risk_cloud)
+    category.append(category_local)
+    category.append('unknown') if category_local is 'not-resolved' else category.append(risk_local)
+
+    return category
+
 
 # Reads in a text file, parses the url/domain, & returns a list of strings
 def parse_text_file(infile):
@@ -42,49 +100,42 @@ def cli(target_ip, target_port, target_username, target_password, verify_type, i
         print("Error: Invalid type of check. Must use either 'domain' or 'url'.")
         exit()
 
-
     # Gather domains/urls to parse by either reading from text
     # file or gathering from comma separated list
     if input_locale == 'use text file':
-            separated_list = parse_text_file('input_list.txt')
+        separated_list = parse_text_file('input_list.txt')
     else:
         # use the -i option and a comma separated list of urls
         # useful to spot test a url without reading the file
         separated_list = input_locale.split(',')
         print('\n')
 
-
-    # creates a firewall object based on skilletlib and pan-python
-    print('getting firewall API key\n')
+    # Creates a firewall object based on skilletlib and pan-python
+    print("Getting firewall API key.\n")
     device = Panos(api_username=target_username,
                    api_password=target_password,
                    hostname=target_ip,
-                   api_port=target_port
-                   )
+                   api_port=target_port)
 
     print('URL, cloud category, cloud risk, local category, local risk')
     print('-----------------------------------------------------------\n')
 
+    # Iterate through each domain/url to get category & save to csv
     for item in separated_list:
-        # query the device object to get the url category
-        cli_cmd = f'<test><url>{item}</url></test>'
-        response = device.execute_op(cmd_str=cli_cmd, cmd_xml=False).split('\n')
-        # split the response into local and cloud
-        local = response[0]
-        cloud = response[1]
-        # grab the category and risk values
-        categoryLocal, riskLocal = local.split(" ")[1], local.split(" ")[2]
-        categoryCloud, riskCloud = cloud.split(" ")[1], cloud.split(" ")[2]
-        if categoryLocal == 'not-resolved':
-            # no risk value return so skip the value
-            print(f'{item}, {categoryCloud}, {riskCloud}, {categoryLocal}, unknown')
-        else:
-            print(f'{item}, {categoryCloud}, {riskCloud}, {categoryLocal}, {riskLocal}, ')
+        category = []
+        # Query device object to get the category as a list
+        if verify_type is "domain":
+            category = query_domain(item, device)
+        if verify_type is "url":
+            category = query_url(item, device)
 
-    if verify_type == 'url':
-        print('\nURL checks complete')
-    if verify_type == 'domain':
-        print('\nDomain checks complete')
+        # Write the category list to categories.csv
+        with open('category.csv', 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(category)
+
+    print('\nURL checks complete' if verify_type is 'url' else '\nDomain checks complete')
+
 
 if __name__ == '__main__':
     cli()
